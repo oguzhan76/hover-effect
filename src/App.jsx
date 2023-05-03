c15f682dff4e89b64b24a05d5dad042f17d537b2
delucia <oguzhankukul@gmail.com>
2023-05-03 00:46:19 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import Box from './Box';
+import './styles/style.css'
+
+function App() {
+    const [mousePos, setMousePos] = useState({x:0, y:0});
+
+    const getMousePos = (e) => {
+        setMousePos({ x: e.clientX, y: e.clientY });
+    }
+
+
+  return (
+    <div>
+      <div className="container" onMouseMove={getMousePos}>
+        <Box mousePos={mousePos}/>
+        <Box mousePos={mousePos}/>
+        <Box mousePos={mousePos}/>
+        <Box mousePos={mousePos}/>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Box.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from "react";
+import PropTypes from 'prop-types';
+
+const Box = ({mousePos}) => {
+    const boxref = useRef();
+    const vars = useRef({
+        "--mouse-x": `5000px`,
+        "--mouse-y": `5000px`
+    });
+
+    useEffect(() => {
+        const rect = boxref.current.getBoundingClientRect()
+        const x = mousePos.x - rect.left;
+        const y = mousePos.y - rect.top;
+        vars.current = {
+            "--mouse-x": `${x}px`,
+            "--mouse-y": `${y}px`
+        }
+    })
+
+    return (
+        <div ref={boxref} className="box" style={vars.current} >
+            <div className="box-content" >Lorem Ipsum</div>
+        </div>
+    )
+}
+
+Box.propTypes = {
+    mousePos: PropTypes.object
+}
+
+export default Box;
